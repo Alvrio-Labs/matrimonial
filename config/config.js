d2b65c58270fecb0e91f358dd60ee0b7b2d95db8
Jain-Daksh <daksh@gmail.com>
2022-12-20 20:28:28 +0530

    added nvmrc

--- config/config.js
+++ config/config.js
@@ -2,36 +2,159 @@ require('dotenv').config();
 require('dotenv').config({ path: './.env.development' });
 require('dotenv').config({ path: './.env.test' });
 require('dotenv').config({ path: './.env.production' });
+const dotenv = require('dotenv');
+const path = require('path');
 
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
+if (process.env.NODE_ENV === 'production') {
+  'username' =  process.env.DB_USERNAME,
+  'password' = process.env.DB_PASSWORD,
+  'database' = process.env.DB_NAME,
+  'host' = process.env.DB_HOST,
+  'dialect' = 'postgres',
+  'define' = {
+    'timestamp': false
+  }
+}
+else if (process.env.NODE_ENV === 'test') {
+  'username' = process.env.DB_USERNAME,
+    'password' = process.env.DB_PASSWORD,
+    'database' = process.env.DB_NAME,
+    'host' = process.env.DB_HOST,
+    'dialect' = 'postgres',
+    'define' = {
+      'timestamp': false
+    }
+
+} else {
+  'username' = process.env.DB_USERNAME,
+  'password' = process.env.DB_PASSWORD,
+  'database' = process.env.DB_NAME,
+  'host' = process.env.DB_HOST,
+  'dialect' = 'postgres',
+  'define' = {
+    'timestamp': false
+  }}
 module.exports = {
-  'development': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp' : false
-    }
-  },
-  'production': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp' : false
-    }
-  },
-  'test': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp' : false
-    }
-  },
-};
+  production,development,test
+}
+// module.exports = {
+//   'development': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+//   'production': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+//   'test': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+// };
+
+
+// module.exports = {
+//   NODE_ENV: process.env.NODE_ENV || 'development',
+//   if(){
+    
+//   }
+//   'username': process.env.DB_USERNAME,
+//   'password': process.env.DB_PASSWORD,
+//   'database': process.env.DB_NAME,
+//   'host': process.env.DB_HOST,
+//   'dialect': 'postgres',
+//   'define': {
+//     'timestamp': false
+//   }
+// }
+
+
+// module.exports = {
+//   'development': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+//   'production': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+//   'test': {
+//     'username': process.env.DB_USERNAME,
+//     'password': process.env.DB_PASSWORD,
+//     'database': process.env.DB_NAME,
+//     'host': process.env.DB_HOST,
+//     'dialect': 'postgres',
+//     'define': {
+//       'timestamp' : false
+//     }
+//   },
+// };
+
+// require('dotenv').config();
+// require('dotenv').config({ path: './.env.development' });
+// require('dotenv').config({ path: './.env.test' });
+// require('dotenv').config({ path: './.env.production' });
+
+// const env = process.env.NODE_ENV; // 'dev' or 'test'
+// console.log(process.env.NODE_ENV)
+// // const dev = {
+// //   app: {
+// //     port: 3000
+// //   },
+// //   db: {
+// //     host: 'localhost',
+// //     port: 27017,
+// //     name: 'db'
+// //   }
+// // };
+
+// // const test = {
+// //   'username': process.env.DB_USERNAME,
+// //   'password': process.env.DB_PASSWORD,
+// //   'database': process.env.DB_NAME,
+// //   'host': process.env.DB_HOST,
+// //   'dialect': 'postgres',
+// //   'define': {
+// //     'timestamp': false
+// //   }
+// // };
+
+// // const config = {
+// //   dev,
+// //   test
+// // };
+
+// module.exports = config[env];
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
-require('dotenv').config({path: './.env.development'});
-require('dotenv').config({path: './.env.test'});
-require('dotenv').config({path: './.env.production'});
+// require('dotenv').config();
+// require('dotenv').config({path: './.env.development'});
+// require('dotenv').config({path: './.env.test'});
+// require('dotenv').config({path: './.env.production'});
+const dotenv = require('dotenv');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.SERVER_PORT || 3011;
-
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
