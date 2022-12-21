ac24a53e17494893487527bf5d825cb4dab6f1ad
Jain-Daksh <daksh@gmail.com>
2022-12-21 17:49:59 +0530

    fixed eslint error

--- auth/auth-policy.js
+++ auth/auth-policy.js
@@ -1,26 +1,25 @@
 const JWT = require('jsonwebtoken');
-const _ = require('dotenv')
-
+const _ = require('dotenv');
 const VERIFYTOKEN = async (req, res, next) => {
   if (!req.headers['authorization']) {
-    return res.status(401).send('Token missing')
+    return res.status(401).send('Token missing');
   }
-  var authHeader = req.headers['authorization']
-  var bearerToken = authHeader.split(' ')
-  const token = bearerToken[1]
+  var authHeader = req.headers['authorization'];
+  var bearerToken = authHeader.split(' ');
+  const token = bearerToken[1];
   JWT.verify(token, process.env.SECRET_KEY, (err, payload) => {
     if (err) {
       res.json({
         success: 0,
-        message: "Invalid token"
-      })
+        message: 'Invalid token'
+      });
     }
     else {
-      next()
+      next();
     }
-  })
-}
+  });
+};
 
 module.exports = {
   VERIFYTOKEN
-}
+};
