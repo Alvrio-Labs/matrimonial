5c76b85da946f03ee14837071df27136f50c3f4f
Jain-Daksh <daksh@gmail.com>
2022-12-21 17:15:14 +0530

    added auth

---
+++ auth/auth-policy.js
@@ -0,0 +1,26 @@
+const JWT = require('jsonwebtoken');
+const _ = require('dotenv')
+
+const VERIFYTOKEN = async (req, res, next) => {
+  if (!req.headers['authorization']) {
+    return res.status(401).send('Token missing')
+  }
+  var authHeader = req.headers['authorization']
+  var bearerToken = authHeader.split(' ')
+  const token = bearerToken[1]
+  JWT.verify(token, process.env.SECRET_KEY, (err, payload) => {
+    if (err) {
+      res.json({
+        success: 0,
+        message: "Invalid token"
+      })
+    }
+    else {
+      next()
+    }
+  })
+}
+
+module.exports = {
+  VERIFYTOKEN
+}
