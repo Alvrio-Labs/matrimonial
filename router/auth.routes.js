e13f0e1ec561677e6360d6c9def1c548483b52a4
Jain-Daksh <daksh@gmail.com>
2022-12-26 20:16:27 +0530

    fixed router

--- router/auth.routes.js
+++ router/auth.routes.js
@@ -1,11 +1,11 @@
 module.exports = app => {
-  const UserAuth = require('../controller/auth.controller');
-  const {AuthValidation} = require('../validation/auth.validation');
+  const auth = require('../controllers/auth.controller');
+  const { AuthValidation } = require('../validation/auth.validation');
   const router = require('express').Router();
   
-  router.post('/login', AuthValidation.userLoginValidation, UserAuth.login);
-  router.post('/forget-password', AuthValidation.userForgotPasswordValidation, UserAuth.forgetPassword);
-  router.post('/resetpassword', AuthValidation.userResetPasswordValidation, UserAuth.resetPassword);
+  router.post('/login', AuthValidation.userLoginValidation, auth.login);
+  router.post('/forget-password', AuthValidation.userForgotPasswordValidation, auth.forgetPassword);
+  router.post('/resetpassword', AuthValidation.userResetPasswordValidation, auth.resetPassword);
 
   app.use('/api' , router);
 };
