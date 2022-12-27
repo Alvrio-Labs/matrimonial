97f497f2c27e2cd33090670e4981e9ce8eb70e41
Jain-Daksh <daksh@gmail.com>
2022-12-27 20:28:27 +0530

    removed commented code

--- utility/nodemailer.js
+++ utility/nodemailer.js
@@ -9,37 +9,7 @@ const TRANSPORTER = nodemailer.createTransport(
     }
   }
 );
-const 
+
 module.exports = {
   TRANSPORTER
 };
-
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (email, subject, text) => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.HOST,
-//       service: process.env.SERVICE,
-//       port: 587,
-//       secure: true,
-//       auth: {
-//         user: process.env.USER,
-//         pass: process.env.PASS,
-//       },
-//     });
-
-//     await transporter.sendMail({
-//       from: process.env.USER,
-//       to: email,
-//       subject: subject,
-//       text: text,
-//     });
-
-//     console.log('email sent sucessfully');
-//   } catch (error) {
-//     console.log(error, 'email not sent');
-//   }
-// };
-
-// module.exports = sendEmail;
