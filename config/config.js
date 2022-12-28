37712437a62d128e84556b91ea4b7b046e9ad3b2
Jain-Daksh <daksh@gmail.com>
2022-12-28 12:14:32 +0530

    fixed eslint for config

--- config/config.js
+++ config/config.js
@@ -5,38 +5,38 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
 });
 module.exports = {
-  'development': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
-  'production': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
-  'test': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
 };
 
