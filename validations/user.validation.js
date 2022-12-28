f8fd46ca108ad1e647f28e188702e382ca24347a
Jain-Daksh <daksh@gmail.com>
2022-12-28 13:44:22 +0530

    fixed user validation acc to eslint

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 const fs = require('fs');
 const YAML = require('js-yaml');
+
 const validation = fs.readFileSync('../yaml/validation.yaml');
 const data = YAML.load(validation);
 // schema to create a user
@@ -8,10 +9,13 @@ const createSchema = Joi.object({
   first_name: Joi.string().required().max(data.user.firstName.max).min(data.user.firstName.min),
   last_name: Joi.string().required().max(data.user.lastName.max).min(data.user.lastName.min),
   email: Joi.string().min(3).required().email(),
-  phone: Joi.number().integer().min(data.user.mobile.min).max(data.user.mobile.max).message(data.user.mobile.errorMessage).required(),
+  phone: Joi.number().integer().min(data.user.mobile.min).max(data.user.mobile.max)
+    .message(data.user.mobile.errorMessage)
+    .required(),
   gender: Joi.string().valid(...data.user.gender.type).message(data.user.gender.errorMessage).required(),
   date_of_birth: Joi.string().required(),
-  password: Joi.string().pattern(new RegExp(data.user.password.regex)).min(data.user.password.min).message(data.user.password.errorMessage).required()
+  password: Joi.string().pattern(new RegExp(data.user.password.regex)).min(data.user.password.min).message(data.user.password.errorMessage)
+    .required(),
 });
 // validation with schema to create a user
 const createValidation = async (req, res, next) => {
@@ -47,7 +51,7 @@ const deleteValidation = async (req, res, next) => {
   const value = deleteSchema.validate(req.body);
   if (value.error) {
     res.status(400).json({
-      message: value.error.details[0].message
+      message: value.error.details[0].message,
     });
   } else {
     next();
@@ -58,10 +62,13 @@ const updateSchema = Joi.object({
   first_name: Joi.string().required().max(data.user.firstName.max).min(data.user.firstName.min),
   last_name: Joi.string().required().max(data.user.lastName.max).min(data.user.lastName.min),
   email: Joi.string().min(3).required().email(),
-  phone: Joi.number().integer().min(data.user.mobile.min).max(data.user.mobile.max).message(data.user.mobile.errorMessage).required(),
+  phone: Joi.number().integer().min(data.user.mobile.min).max(data.user.mobile.max)
+    .message(data.user.mobile.errorMessage)
+    .required(),
   gender: Joi.string().valid(...data.user.gender.type).required(),
   date_of_birth: Joi.string().required(),
-  password: Joi.string().pattern(new RegExp(data.user.password.regex)).min(data.user.password.min).message(data.user.password.errorMessage).required()
+  password: Joi.string().pattern(new RegExp(data.user.password.regex)).min(data.user.password.min).message(data.user.password.errorMessage)
+    .required(),
 });
 // validation with schema to create a user
 const updateValidation = async (req, res, next) => {
