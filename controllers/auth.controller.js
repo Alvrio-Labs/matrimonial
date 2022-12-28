0180a160b1ff0f662709e395559a03b5cd25ed90
Jain-Daksh <daksh@gmail.com>
2022-12-28 17:32:58 +0530

    changed auth  method

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,13 +3,14 @@ const bcrypt = require('bcrypt');
 const db = require('../models');
 
 const { User } = db.User;
-const TRANSPORTER = require('../utility/nodemailer');
+const { sendmail } = require('../utility/nodemailer');
 
 exports.forgetPassword = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({
     where: { email: req.body.email },
   });
+
   if (user) {
     const resetToken = jwt.sign(
       { id: req.body.id },
@@ -26,15 +27,14 @@ exports.forgetPassword = async (req, res) => {
         <h1>Clink on link to reset password</h1>
         <p> <a href ="http://localhost:3000/reset-password/${resetToken}"</a> Click here</p>`,
     };
-    TRANSPORTER.sendMail(mailOptions, (err, result) => {
+    sendmail(mailOptions, (err, result) => {
       if (err) {
         console.log(err);
       } else {
         console.log('email sent', result.response);
       }
     });
-    const currentUser = await user.update({ reset_token: resetToken });
-    console.log(currentUser);
+    user.update({ reset_token: resetToken });
   } else {
     return res.status(404).send({
       message: 'No user of this id',
@@ -99,12 +99,3 @@ exports.login = async (req, res) => {
   }
   return null;
 };
-
-
-// const { forgetPassword, login, resetPassword } = require('../utility/mailer');
-
-// exports.forgetPassword = async (req, res) => {
-//   const forgotPasswordService = await forgetPassword(req.body);
-//   return res.json(forgotPasswordService);
-// };
-
--- utility/nodemailer.js
+++ utility/nodemailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const _ = require('dotenv');
 
-const TRANSPORTER = nodemailer.createTransport(
+const sendmail = nodemailer.createTransport(
   {
     service: 'gmail',
     auth: {
@@ -12,50 +12,6 @@ const TRANSPORTER = nodemailer.createTransport(
 );
 
 module.exports = {
-  TRANSPORTER,
+  sendmail,
 };
 
-
-// const nodemailer = require('nodemailer');
-// const fs = require('fs');
-// const path = require('path');
-
-// const sendEmail = async (email, subject, link) => {
-//   try {
-//     // create reusable transporter object using the default SMTP transport
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.EMAIL_HOST,
-//       port: 465,
-//       auth: {
-//         user: process.env.EMAIL_USERNAME,
-//         pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
-//       },
-//     });
-
-//     const options = () => ({
-//       from: process.env.FROM_EMAIL,
-//       to: email,
-//       subject: subject,
-//       html: link,
-//     });
-
-//     // Send email
-//     transporter.sendMail(options(), (error, res) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         return res.status(200).json({
-//           success: true,
-//         });
-//       }
-//       return null;
-//     });
-//   } catch (error) {
-//     return error;
-//   }
-//   return null;
-// };
-
-// module.exports = {
-//   sendEmail,
-// };
