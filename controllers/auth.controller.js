76da6737baab905494f43452efe264a8b5a1ca27
Jain-Daksh <daksh@gmail.com>
2022-12-28 12:59:33 +0530

    fixed auth validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,25 +1,30 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const db = require('../models');
-const User = db.User;
+
+const { User } = db.User;
 const TRANSPORTER = require('../utility/nodemailer');
 
 exports.forgetPassword = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({
-    where: { email: req.body.email }
+    where: { email: req.body.email },
   });
   if (user) {
-    const resetToken = jwt.sign({ id: req.body.id }, process.env.RESET_PASSWORD_KEY, {
-      expiresIn: process.env.EXPIRY_IN
-    });
+    const resetToken = jwt.sign(
+      { id: req.body.id },
+      process.env.RESET_PASSWORD_KEY,
+      {
+        expiresIn: process.env.EXPIRY_IN,
+      },
+    );
     const mailOptions = {
       from: process.env.EMAIL,
       to: email,
       subject: 'To Change Password',
       html: `
         <h1>Clink on link to reset password</h1>
-        <p> <a href ="http://localhost:3000/reset-password/${resetToken}"</a> Click here</p>`
+        <p> <a href ="http://localhost:3000/reset-password/${resetToken}"</a> Click here</p>`,
     };
     TRANSPORTER.sendMail(mailOptions, (err, result) => {
       if (err) {
@@ -32,7 +37,7 @@ exports.forgetPassword = async (req, res) => {
     console.log(currentUser);
   } else {
     return res.status(404).send({
-      message: 'No user of this id'
+      message: 'No user of this id',
     });
   }
   return '';
@@ -44,29 +49,27 @@ exports.resetPassword = async (req, res) => {
     jwt.verify(resetToken, process.env.RESET_PASSWORD_KEY).then((err, token) => {
       if (err) {
         return res.status(401).json({
-          message: 'Incorrect or expired'
+          message: 'Incorrect or expired',
         });
       }
 
       User.findOne({ where: { reset_token: token } }).then((error, user) => {
         if (error) {
           return res.status(400).json({
-            error: 'User with this token does not exist'
+            error: 'User with this token does not exist',
           });
         }
         if (user) {
-          user.update({ password: newPassword, reset_token: '' }).then(() => {
-            return res.status(200).send({
-              message: 'Password update'
-            });
-          });
+          user.update({ password: newPassword, reset_token: '' }).then(() => res.status(200).send({
+            message: 'Password update',
+          }));
         }
       });
       return null;
     });
   } else {
     return res.status(404).send({
-      message: 'User with this id does not exist'
+      message: 'User with this id does not exist',
     });
   }
   return null;
@@ -79,18 +82,18 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, result.password);
     if (result.email === email && isMatch) {
       const jwtToken = jwt.sign({ isMatch: result }, process.env.SECRET_KEY, {
-        expiresIn: process.env.EXPIRY_IN
+        expiresIn: process.env.EXPIRY_IN,
       });
-      return res.json({
+      res.json({
         message: 'Successful',
-        token: jwtToken
+        token: jwtToken,
       });
     } else {
-      res.status(401).send('email or password not vaild');
+      return res.status(401).send('email or password not vaild');
     }
   } else {
     return res.status(404).send({
-      message: 'User not found'
+      message: 'User not found',
     });
   }
   return null;
