4137a1e389818b4f55a94ef60fda2f87ff5bf093
Jain-Daksh <daksh@gmail.com>
2022-12-28 21:04:31 +0530

    fixed acc to eslint

--- config/config.js
+++ config/config.js
@@ -1,45 +1,3 @@
-// require('dotenv').config({ path: './.env.development' });
-// require('dotenv').config({ path: './.env.test' });
-// require('dotenv').config({ path: './.env.production' });
-// const dotenv = require('dotenv');
-// const path = require('path');
-
-// dotenv.config({
-//   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
-// });
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     define: {
-//       timestamp: false,
-//     },
-//   },
-//   production: {
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     define: {
-//       timestamp: false,
-//     },
-//   },
-//   test: {
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     define: {
-//       timestamp: false,
-//     },
-//   },
-// };
-
 require('dotenv').config();
 require('dotenv').config({ path: './.env.development' });
 require('dotenv').config({ path: './.env.test' });
@@ -51,35 +9,34 @@ dotenv.config({
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
 });
 module.exports = {
-  'development': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
-  'production': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
-  'test': {
-    'username': process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'database': process.env.DB_NAME,
-    'host': process.env.DB_HOST,
-    'dialect': 'postgres',
-    'define': {
-      'timestamp': false
-    }
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamp: false,
+    },
   },
 };
-
