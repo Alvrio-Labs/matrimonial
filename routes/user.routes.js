42f016e597817720f8bc007b14ca3e1c08adf04a
Jain-Daksh <daksh@gmail.com>
2023-01-02 22:18:12 +0530

    removed commented code

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,3 @@
-// const router = require('express').Router();
-// const { verifyToken } = require('../utility/auth.policy');
-// const User = require('../controllers/users.controller');
-
-// module.exports = (app) => {
-//   router.post('/', User.create);
-//   router.get('/:id', User.findOne);
-//   router.put('/:id', verifyToken, User.update);
-//   router.delete('/:id', User.delete);
-//   app.use('/api/users', router);
-// };
-
 const router = require('express').Router();
 const { verifyToken } = require('../utility/auth.policy');
 const User = require('../controllers/users.controller');
