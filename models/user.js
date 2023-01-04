3a2d8c9296533700e6cf28ea307e2758ebaa5aae
Jain-Daksh <daksh@gmail.com>
2023-01-04 20:54:32 +0530

    removed bcrypt password compare method

--- models/user.js
+++ models/user.js
@@ -77,9 +77,6 @@ module.exports = (sequelize) => {
     if (ageCheck < birthDate) {
       throw new Error('Age must be above 18+');
     }
-    const comparePassword = function (password) {
-      return bcrypt.compareSync(password, model.password);
-    };
   });
 
   return User;
