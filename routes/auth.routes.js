6c99fac5abe575f03e15b0c75a6ec4e7a43e95a5
Jain-Daksh <daksh@gmail.com>
2023-01-05 12:45:44 +0530

    routes name changed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,24 @@
+// const router = require('express').Router();
+
+// const auth = require('../controllers/auth.controller');
+// const { resetPasswordValidation, forgotPasswordValidation, loginValidation } = require('../validations/auth.validation');
+
+// module.exports = (app) => {
+//   router.post('/login', auth.login);
+//   router.post('/forget-password', auth.forgetPassword);
+//   router.post('/reset-password', auth.resetPassword);
+
+//   app.use('/api', router);
+// };
 const router = require('express').Router();
 
 const auth = require('../controllers/auth.controller');
-const { resetPasswordValidation, forgotPasswordValidation, loginValidation } = require('../validations/auth.validation');
+const { resetPassword, forgotPassword, login } = require('../validations/auth.validation');
 
 module.exports = (app) => {
-  router.post('/login', auth.login);
-  router.post('/forget-password', auth.forgetPassword);
-  router.post('/reset-password', auth.resetPassword);
+  router.post('/login', login, auth.login);
+  router.post('/forget-password', forgotPassword, auth.forgetPassword);
+  router.post('/reset-password', resetPassword, auth.resetPassword);
 
   app.use('/api', router);
 };
