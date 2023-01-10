b342114ba561dc94250150ec5b89e51bf93abe44
Jain-Daksh <daksh@gmail.com>
2023-01-10 15:04:43 +0530

    added token

--- routes/education_details.routes.js
+++ routes/education_details.routes.js
@@ -3,7 +3,7 @@ const EI = require('../controllers/education_details.controller');
 const { verifyToken } = require('../policy/auth.policy');
 
 module.exports = (app) => {
-  router.post('/', EI.create);
+  router.post('/', verifyToken, EI.create);
   router.get('/:id', verifyToken, EI.show);
   router.put('/:id', verifyToken, EI.update);
   router.delete('/:id', verifyToken, EI.delete);
--- routes/life_style.routes.js
+++ routes/life_style.routes.js
@@ -3,7 +3,7 @@ const LS = require('../controllers/life_style.controller');
 const { verifyToken } = require('../policy/auth.policy');
 
 module.exports = (app) => {
-  router.post('/', LS.create);
+  router.post('/', verifyToken, LS.create);
   router.get('/:id', verifyToken, LS.show);
   router.put('/:id', verifyToken, LS.update);
   router.delete('/:id', verifyToken, LS.delete);
