b243be94fa35b0f7f7249e3764880a32b005c9ee
Jain-Daksh <daksh@gmail.com>
2023-01-19 20:02:38 +0530

    added pagination

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -74,7 +74,103 @@ exports.delete = async (req, res) => {
   }
 };
 
+// exports.filter_index = async (req, res) => {
+//   try {
+//     const user = await User.findByPk(req.params.id, {
+//       include: [{
+//         model: UserPreference,
+//         as: 'user_preference',
+//       },
+//       {
+//         model: FamilyInfo,
+//         as: 'family_info',
+//       },
+//       {
+//         model: EducationInfo,
+//         as: 'education_info',
+//       },
+//       {
+//         model: lifeStyle,
+//         as: 'life_style',
+//       },
+//       {
+//         model: PersonalInfo,
+//         as: 'personal_info',
+//       }],
+//     });
+//     const userData = user.toJSON();
+
+//     const users = await User.findAll({
+//       include: [
+//         {
+//           model: FamilyInfo,
+//           as: 'family_info',
+//           where: {
+//             city: {
+//               [Op.or]: userData.user_preference.city,
+//             },
+//           },
+//         },
+//         {
+//           model: PersonalInfo,
+//           as: 'personal_info',
+//           where: {
+//             manglik: {
+//               [Op.or]: userData.user_preference.manglik,
+//             },
+//           },
+//         },
+//         {
+//           model: lifeStyle,
+//           as: 'life_style',
+//         },
+//         {
+//           model: EducationInfo,
+//           as: 'education_info',
+//         },
+//       ],
+//       where: {
+//         id: {
+//           [Op.ne]: user.id,
+//         },
+//         gender: {
+//           [Op.ne]: user.gender,
+//         },
+//       },
+//     });
+//     const responseData = await serialize.filter(users);
+//     res.status(200).send({
+//       user_preference: responseData,
+//     });
+//   } catch (error) {
+//     res.status(404).send({
+//       message: error.message,
+//     });
+//   }
+// };
+
+const getPagination = (page, size) => {
+  const limit = size ? +size : 5;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+const getPagingData = (data, page, limit) => {
+  const { count: totalItems, rows: tutorials } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return {
+    totalItems, tutorials, totalPages, currentPage,
+  };
+};
+
 exports.filter_index = async (req, res) => {
+  const { page, size, id } = req.query;
+  const condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
+
+  const { limit, offset } = getPagination(page, size);
+
   try {
     const user = await User.findByPk(req.params.id, {
       include: [{
@@ -99,15 +195,23 @@ exports.filter_index = async (req, res) => {
       }],
     });
     const userData = user.toJSON();
-
+    console.log(userData);
     const users = await User.findAll({
+      where: {
+        gender: {
+          [Op.ne]: user.gender,
+        },
+        id: {
+          [Op.ne]: user.id,
+        },
+      },
       include: [
         {
           model: FamilyInfo,
           as: 'family_info',
           where: {
             city: {
-              [Op.or]: userData.user_preference.city,
+              [Op.like]: userData.user_preference.city,
             },
           },
         },
@@ -129,16 +233,23 @@ exports.filter_index = async (req, res) => {
           as: 'education_info',
         },
       ],
-      where: {
-        id: {
-          [Op.ne]: user.id,
-        },
-        gender: {
-          [Op.ne]: user.gender,
-        },
-      },
+      offset: offset,
+      limit: limit,
+      condition: condition,
+      // where: {
+
+      //   id: {
+      //     [Op.ne]: user.id,
+      //   },
+      //   gender: {
+      //     [Op.ne]: user.gender,
+      //   },
+      // },
     });
-    const responseData = await serialize.filter(users);
+    console.log('usr', users);
+    const keys = Object.values(users);
+
+    const responseData = await serialize.filter(keys);
     res.status(200).send({
       user_preference: responseData,
     });
