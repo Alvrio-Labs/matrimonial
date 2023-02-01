4fcb7979ee227a2dd7da30be8665fb5025ff84c8
Jain-Daksh <daksh@gmail.com>
2023-02-01 13:19:34 +0530

    added socket.io

--- server.js
+++ server.js
@@ -6,21 +6,35 @@ const dotenv = require('dotenv');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.SERVER_PORT || 3011;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+const io = require('socket.io')(server);
 const { router } = require('./routes/index');
 const db = require('./models');
 const { sequelize } = require('./models');
 
-db.sequelize.sync({ force: false });
-const app = express();
-const PORT = process.env.SERVER_PORT || 3011;
 dotenv.config({
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+app.use(express.static(`${__dirname}/public`));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', msg);
+  });
 });
---
+++ utilities/socket.io.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-invalid-this */
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io');
+// const message = require('../controllers/message.controller');
+
+app.set('port', 4000);
+
+io.on('connection', (socket) => {
+  socket.on('join', async (chat) => {
+    socket.join(chat);
+    io.emit('chat-joined');
+    console.log('chat joined');
+  });
+
+  socket.on('message', async (data) => {
+    io.emit('newMessage', data);
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('stopTyping');
+    socket.on('user_join', function (data) {
+      this.username = data;
+      socket.broadcast.emit('user_join', data);
+    });
+
+    socket.on('chat_message', function (data) {
+      // eslint-disable-next-line no-param-reassign
+      data.username = this.username;
+      socket.broadcast.emit('chat_message', data);
+    });
+
+    socket.on('disconnect', function (data) {
+      socket.broadcast.emit('user_leave', this.username);
+    });
+  });
+});
+
+server.listen(4000);
