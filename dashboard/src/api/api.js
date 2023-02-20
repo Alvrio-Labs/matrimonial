ad99c8298e8c24ac9145049daee592a6267d66e4
Jain-Daksh <daksh@gmail.com>
2023-02-20 11:16:38 +0530

    removed unused file

--- dashboard/src/api/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const users = (count, page) =>
-  axios.get(`http://localhost:3001/api/admin/users?count=${count}&page=${page}`);
-  // http://localhost:3000/api/admin/users?page=1
