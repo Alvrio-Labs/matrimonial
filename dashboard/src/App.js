a43db5bb4dc70bee15888d454dc84992e8dc3de2
Jain-Daksh <daksh@gmail.com>
2023-02-23 17:33:09 +0530

    login form

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
@@ -8,22 +8,36 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Homepage from "./components/Homepage";
+import Login from "./components/Login";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const SidebarLayout = () => (
+    <>
+      {/* <Sidebar isSidebar={isSidebar}/> */}
+      <div className="app">
+        <Sidebar isSidebar={isSidebar} />
+        <main className="content">
+          <Topbar setIsSidebar={setIsSidebar} />
+        </main>
+      </div>
+      <Outlet />
+    </>
+  );
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-
+        
         <div className="app">
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Form />} />
+              <Route path="/" element={<Login />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
@@ -32,6 +46,19 @@ function App() {
             </Routes>
           </main>
         </div>
+
+        {/* <div className="app">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route element={<SidebarLayout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/form" element={<Form />} />
+            </Route>
+
+          </Routes>
+        </div> */}
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
