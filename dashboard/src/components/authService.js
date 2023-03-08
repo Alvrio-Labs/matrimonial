0c1a85a310347844b0e4228e7da78227d30c2721
Jain-Daksh <daksh@gmail.com>
2023-03-08 16:05:30 +0530

    removed commented code

--- dashboard/src/components/authService.js
+++ dashboard/src/components/authService.js
@@ -1,33 +1,3 @@
-// import axios from "axios";
-
-// const login = async (email, password) => {
-//   return await axios
-//     .post("http://localhost:5000/api/login", {
-//       email,
-//       password,
-//     })
-//     .then((response) => {
-//       console.log(response.data)
-//       localStorage.setItem("authtoken", response.data.token);
-//       return response.data;
-//     });
-// };
-
-// const logout = () => {
-//   localStorage.removeItem("authtoken");
-// };
-
-// const getCurrentUser = () => {
-//   return JSON.parse(localStorage.getItem("authtoken"));
-// };
-
-// const authService = {
-//   login,
-//   logout,
-//   getCurrentUser,
-// };
-
-// export default authService;
 
 import axios from "axios";
 
