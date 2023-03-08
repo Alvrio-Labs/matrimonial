0e8962228d1ad4aa6b736e3c7c19e59c62a39261
Jain-Daksh <daksh@gmail.com>
2023-03-08 16:06:21 +0530

    removed commented code

--- dashboard/src/components/authHeaders.js
+++ dashboard/src/components/authHeaders.js
@@ -1,6 +1,5 @@
 export default function authHeader() {
   const token = localStorage.getItem("authtoken");
-  console.log(token)
   if (token) {
     return { Authorization: `Bearer ${token}` };
     // return  `Bearer ${token}`;
