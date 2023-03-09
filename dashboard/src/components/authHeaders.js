abd8a71611964bdc385a8b2028a399b5b197df55
Jain-Daksh <daksh@gmail.com>
2023-03-09 15:41:38 +0530

    removed commented code of bearer code

--- dashboard/src/components/authHeaders.js
+++ dashboard/src/components/authHeaders.js
@@ -1,7 +1,6 @@
 export default function authHeader() {
   const token = localStorage.getItem("authtoken");
   if (token) {
-    // return { Authorization: `Bearer ${token}` };
     return  `Bearer ${token}`;
 
   } else {
