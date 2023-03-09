df22e7228ba590ee562c29d60fef125ed7713685
Jain-Daksh <daksh@gmail.com>
2023-03-09 16:02:49 +0530

    removed commented code

--- dashboard/src/components/authUserLink.js
+++ dashboard/src/components/authUserLink.js
@@ -1,29 +1,6 @@
-// import axios from "axios";
 import axios from "./api/baseUrl";
-
 import authHeader from "./authHeaders";
 
-// const getAllUser = () => {
-//   return axios.get("http://localhost:5000/api/admin/users", {
-//     headers: {
-//       "Authorization": authHeader()
-//     }
-//   }
-//   );
-// };
-// const getSingleUser = ({ user }) => {
-//   return axios.get(`http://localhost:5000/api/admin/users/${user.id}`,
-//     { headers: authHeader() }
-//   )
-// }
-// const addUser = () => {
-//   return axios.post('http://localhost:5000/api/admin/users', {
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Authorization": authHeader(),
-//     },
-//   });
-// }
 const getAllUser = () => {
   return axios.get("users", {
     headers: {
@@ -32,11 +9,13 @@ const getAllUser = () => {
   }
   );
 };
+
 const getSingleUser = ({ user }) => {
   return axios.get(`/${user.id}`,
     { headers: authHeader() }
   )
-}
+};
+
 const addUser = () => {
   return axios.post('users', {
     headers: {
@@ -44,7 +23,8 @@ const addUser = () => {
       "Authorization": authHeader(),
     },
   });
-}
+};
+
 const authUserLink = {
   getAllUser, addUser, getSingleUser
 };
